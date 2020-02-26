6326606d72c5c171c8aa5edfe94ab307e986b49f
Abhinav Trivedi <abhinav@darkbears.com>
2020-02-26 17:32:27 +0530

    display all questions

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -8,7 +8,7 @@ use Faker\Generator as Faker;
 $factory->define(App\Question::class, function (Faker $faker) {
     return [
     	'title' => rtrim($faker->sentence(rand(5,10)), "."),
-    	'body' => $faker->paragraphs(rand(3, 7). true),
+    	'body' => $faker->paragraphs(rand(3, 7). true)[0],
     	'views' => rand(0,10),  
     	'answers' => rand(0,10),
     	'votes' => rand(-3, 10)  
